8305155f33b64d08bc650c5fa792d09d69d8fca2
wkm23@126.com <wkm23@126.com>
2016-11-13 15:51:13 +1300

    WK-001: node,express,mongo work

---
+++ modules/core/server/controllers/core.server.controller.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Kyle on 11/13/2016.
+ */
+'use strict';
+
+exports.renderIndex = function (req, res) {
+    res.render('modules/core/server/views/index');
+};
+    
+
+ 
---
+++ modules/core/server/routes/core.server.routes.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+module.exports = function (app) {
+    var core = require('../controllers/core.server.controller');
+    app.route('/*').get(core.renderIndex);
+};
+
+ 
